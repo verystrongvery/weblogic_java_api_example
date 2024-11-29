b447b7609daccebf1e11809be71f3996b156b9cd
kimjinbeom <jinbeom_kim@tmax.co.kr>
2024-11-29 10:29:17 +0900

    feat: 간단한 서블릿 프로젝트 구현

---
+++ src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello") // URL 패턴 설정
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body></html>");
+    }
+}
